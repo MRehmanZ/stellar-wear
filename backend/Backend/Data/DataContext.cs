79801fa80b50c41acf691367318879c0f0688020
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-08-19 12:15:23 +0100

    chore: organising project; adding backend

---
+++ backend/Backend/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Backend.Models; 
+
+namespace Backend.Data 
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+}

321d958bc4668cac9e16e0f91f5e67374f823725
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-08-21 14:33:42 +0100

    feat: implement video landing page on auto loop

--- frontend/src/components/Heroes.jsx
+++ frontend/src/components/Heroes.jsx
@@ -4,14 +4,18 @@ import './styles/heroes.css';
 const Heroes = ({ title, subtitle, imageUrl, linkUrl }) => {
   return (
     <div className="hero-container">
-      <img className="heroes-image" src={imageUrl} alt="Hero Image" />
-      <div className="text-on-image">
+      <video className="heroes-video" autoPlay loop muted playsInline>
+        <source src="/LandingVideoPerfected.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="text-on-media">
         <h1>{title}</h1>
         <p>{subtitle}</p>
         <a href={linkUrl} className="heroes-button">Shop Now</a>
       </div>
+      <img className="heroes-image" src={imageUrl} alt="Hero Image" />
     </div>
   );
-}
+};
 
 export default Heroes;

d72c9f759d1ecae8aaa91b73f832b64d5b449c17
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-09-03 23:52:50 +0100

    chore(build): create dockerfile

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9443450a6c523415c42911a7a1f1d545595ba930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab70f08f847d16f26d75615e18dbb8e40092a5f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
